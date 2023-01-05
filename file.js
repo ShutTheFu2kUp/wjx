bdb598b33c6c1f3fc7bf584000e256103e960ed2
ShutTheFu2kUp <40311044+ShutTheFu2kUp@users.noreply.github.com>
2023-01-05 15:33:47 +0800

    Create file.js

---
+++ file.js
@@ -0,0 +1,25 @@
+[{
+    "key": "1. 您的性别：",
+    "content": "男"
+}, {
+    "key": "2. 年龄",
+    "content": "29"
+}, {
+    "key": "",
+    "content": ""
+}, {
+    "key": "",
+    "content": ""
+}, {
+    "key": "",
+    "content": ""
+}, {
+    "key": "",
+    "content": ""
+}, {
+    "key": "",
+    "content": ""
+}, {
+    "key": "",
+    "content": ""
+}]
